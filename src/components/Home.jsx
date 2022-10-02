7aeb4cfbe6352b1f77e74f85510c07f90015c6f6
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2022-10-01 22:02:30 -0400

    added home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, I'm</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Abelaash Giritharan.
+          Abelaash Giritharan
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm second year Humber College

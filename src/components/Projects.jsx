85941914ad6d12897c07a69e940a36d4587127e8
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2023-04-29 13:03:05 -0400

    add projects to repo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import WorkImg from '../assets/workImg.PNG';
 import ReactGame from "../assets/reactGame.png";
 import CEYA from "../assets/ceya.PNG";
+import MERN1 from "../assets/MERN1.png"
+import MERN2 from "../assets/MERN2.png";
+import Recipe from "../assets/RecipeApp.png";
+import Movie from "../assets/MovieApp.png";
+
+
 
 const Projects = () => {
   return (
@@ -69,6 +75,84 @@ const Projects = () => {
             </span>
             <div className='pt-8 text-center'>
               <a href='/'>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        
+        
+
+        <div
+          style={{ backgroundImage: `url(${MERN1})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+        >
+          {/* Hover Effects */}
+          <div className='opacity-0 group-hover:opacity-100'>
+            <span className='text-2xl font-bold text-white tracking-wider '>
+              MERN Assignment
+            </span>
+            <div className='pt-8 text-center'>
+              <a href='https://github.com/Abelaash/Assignment-1_Students-Courses'>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{ backgroundImage: `url(${MERN2})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+        >
+          {/* Hover Effects */}
+          <div className='opacity-0 group-hover:opacity-100'>
+            <span className='text-2xl font-bold text-white tracking-wider '>
+              MERN Final Project
+            </span>
+            <div className='pt-8 text-center'>
+              <a href='https://github.com/Abelaash/Moviestore_Project'>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{ backgroundImage: `url(${Movie})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+        >
+          {/* Hover Effects */}
+          <div className='opacity-0 group-hover:opacity-100'>
+            <span className='text-2xl font-bold text-white tracking-wider '>
+              Movie App
+            </span>
+            <div className='pt-8 text-center'>
+              <a href='https://github.com/Abelaash/React_Native-MovieApp-2023'>
+                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{ backgroundImage: `url(${Recipe})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+        >
+          {/* Hover Effects */}
+          <div className='opacity-0 group-hover:opacity-100'>
+            <span className='text-2xl font-bold text-white tracking-wider '>
+              Recipe App
+            </span>
+            <div className='pt-8 text-center'>
+              <a href='https://github.com/Abelaash/ReactNative_FinalProject'>
                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                   Code
                 </button>
